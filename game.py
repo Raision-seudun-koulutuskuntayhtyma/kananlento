91220e90f098398bf0b9e5b1825d3258db474cab
Tuomas Suutari <tuomas@nepnep.net>
2023-03-27 11:45:12 +0300

    Muuta luokaksi

--- game.py
+++ game.py
@@ -2,24 +2,39 @@ import pygame
 
 
 def main():
-    pygame.init()
-    
-    screen = pygame.display.set_mode((800, 600))
-    clock = pygame.time.Clock()
+    game = Game()
+    game.run()
 
-    running = True
 
-    while running:
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.running = False
+
+    def run(self):
+        clock = pygame.time.Clock()
+        self.running = True
+        while self.running:
+            self.handle_events()
+            self.handle_game_logic()
+            self.update_screen()
+            # Odota niin kauan, että ruudun päivitysnopeus on 60fps
+            clock.tick(60)
+
+        pygame.quit()
+
+    def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                self.running = False
 
-        screen.fill((230, 230, 255))
+    def handle_game_logic(self):
+        pass
+
+    def update_screen(self):
+        self.screen.fill((230, 230, 255))
         pygame.display.flip()
-
-        clock.tick(60)  # Odota niin kauan, että ruudun päivitysnopeus on 60fps
-
-    pygame.quit()
 
 
 if __name__ == "__main__":
