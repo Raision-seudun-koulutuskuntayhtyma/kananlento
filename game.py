432ea85aff11d08873cd3e878edfc2b06c2cb54d
Tuomas Suutari <tuomas@nepnep.net>
2023-03-27 13:58:45 +0300

    Lisää linnulle noste

--- game.py
+++ game.py
@@ -19,7 +19,9 @@ class Game:
         self.img_bird1 = pygame.transform.rotozoom(img_bird1, 0, 1/16)
 
     def init_objects(self):
-        self.bird_pos = (0, 300)
+        self.bird_y_speed = 0
+        self.bird_pos = (200, 000)
+        self.bird_lift = False
 
     def run(self):
         clock = pygame.time.Clock()
@@ -39,9 +41,29 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key in (pygame.K_SPACE, pygame.K_UP):
+                    self.bird_lift = True
+            elif event.type == pygame.KEYUP:
+                if event.key in (pygame.K_SPACE, pygame.K_UP):
+                    self.bird_lift = False
 
     def handle_game_logic(self):
-        self.bird_pos = (self.bird_pos[0] + 1, self.bird_pos[1])
+        bird_y = self.bird_pos[1]
+
+        if self.bird_lift:
+            # Lintua nostetaan (8 px / frame)
+            self.bird_y_speed -= 1
+        else:
+            # Painovoima (lisää putoamisnopeutta joka kuvassa)
+            self.bird_y_speed += 0.2
+
+
+        # Liikuta lintua sen nopeuden verran
+        bird_y += self.bird_y_speed
+
+
+        self.bird_pos = (self.bird_pos[0], bird_y)
 
     def update_screen(self):
         # Täytä tausta vaaleansinisellä
