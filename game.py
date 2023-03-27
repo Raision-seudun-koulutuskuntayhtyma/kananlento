b1c6fd9f180302662c18cb0fe638c173b4beba80
Tuomas Suutari <tuomas@nepnep.net>
2023-03-27 13:23:17 +0300

    Piirrä lintu

--- game.py
+++ game.py
@@ -11,6 +11,11 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
         self.running = False
+        self.init_graphics()
+
+    def init_graphics(self):
+        img_bird1 = pygame.image.load("images/chicken/flying/frame-1.png")
+        self.img_bird1 = pygame.transform.rotozoom(img_bird1, 0, 1/16)
 
     def run(self):
         clock = pygame.time.Clock()
@@ -35,7 +40,12 @@ class Game:
         pass
 
     def update_screen(self):
+        # Täytä tausta vaaleansinisellä
         self.screen.fill((230, 230, 255))
+
+        # Piirrä lintu
+        self.screen.blit(self.img_bird1, (100, 100))
+
         pygame.display.flip()
 
 
